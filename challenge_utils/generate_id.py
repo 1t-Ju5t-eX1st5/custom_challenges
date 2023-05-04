dc8c413263e780ece8d1eec5dc8a35a9943e111a
Solitude Nox <64062496+IronForce-Auscent@users.noreply.github.com>
2023-05-04 08:31:28 +0000

    Deleted challenge_utils files

--- challenge_utils/generate_id.py
+++
@@ -1,6 +0,0 @@
-import hashlib, datetime
-
-def generate_id(file_name: str) -> str:
-    to_hash = file_name + (__file__)
-    hashed_id = hashlib.sha256(to_hash.encode()).hexdigest()
-    return hashed_id[:16]
