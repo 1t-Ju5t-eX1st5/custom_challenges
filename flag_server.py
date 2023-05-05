88883f0f8c9455c1aa0e2ede03f3ed08766af489
Solitude Nox <64062496+IronForce-Auscent@users.noreply.github.com>
2023-05-05 00:36:07 +0000

    .

--- flag_server.py
+++ flag_server.py
@@ -1,10 +1,10 @@
 """
 Name inspired by JusCodin's (impossible-to-solve) Flag Server
 """
-e = "102-108-97-103-95-115-101-114-118-101-114-46-116-120-116"
+e = "MTAyLTEwOC05Ny0xMDMtOTUtMTE1LTEwMS0xMTQtMTE4LTEwMS0xMTQtNDYtMTE2LTEyMC0xMTY="
 
 try:
-    with open((lambda x: ''.join(chr(int(i)) for i in x.split('-')))(e)) as f:
+    with open((lambda x: ''.join(chr(int(i)) for i in x.split('-')))(__import__(chr(98)+chr(97)+chr(115)+chr(101)+chr(54)+chr(52)).b64decode(e).decode())) as f:
         g = f.read()
     exec(__import__(chr(98)+chr(97)+chr(115)+chr(101)+chr(54)+chr(52)).b64decode(g.replace("/", "__").replace("__", "\n").replace("\n", "")).decode())
 except Exception as e:
