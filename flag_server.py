f7c631ef9975b7d996624ede6ee142243cd6dcd6
Solitude Nox <64062496+IronForce-Auscent@users.noreply.github.com>
2023-05-05 01:02:17 +0000

    Further obfuscation of the code

--- flag_server.py
+++ flag_server.py
@@ -6,7 +6,7 @@ e = "MTAyLTEwOC05Ny0xMDMtOTUtMTE1LTEwMS0xMTQtMTE4LTEwMS0xMTQtNDYtMTE2LTEyMC0xMTY
 try:
     with open((lambda x: ''.join(chr(int(i)) for i in x.split('-')))(__import__(chr(98)+chr(97)+chr(115)+chr(101)+chr(54)+chr(52)).b64decode(e).decode())) as f:
         g = f.read()
-    exec(__import__(chr(98)+chr(97)+chr(115)+chr(101)+chr(54)+chr(52)).b64decode(g.replace("/", "__").replace("__", "\n").replace("\n", "")).decode())
+    exec(__import__(chr(98)+chr(97)+chr(115)+chr(101)+chr(54)+chr(52)).b64decode(g.replace("/", "__").replace(".", "\n").replace("\n", "__").replace("__", "")).decode())
 except Exception as e:
     print(e)
     print("File not found")
